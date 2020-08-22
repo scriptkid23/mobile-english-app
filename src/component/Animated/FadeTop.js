c2f1439dbf030cbdbee6b9f2acb075f741b5aa97
HoanDo <sieunhannhenkutesn@gmail.com>
2020-08-22 19:31:56 +0700

    update animation

---
+++ src/component/Animated/FadeTop.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from 'react';
+import { Animated } from 'react-native';
+
+export default (props) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+ 
+  React.useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: props.duration,
+      }
+    ).start();
+  }, [fadeAnim])
+
+  return (
+    <Animated.View                
+     style={{
+      opacity: fadeAnim, 
+      transform: [{
+        translateY: fadeAnim.interpolate({
+          inputRange: [0, 1],
+          outputRange: [-150,0]  
+        }),
+      }],
+    }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
