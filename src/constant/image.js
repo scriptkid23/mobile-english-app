109bacb029b13b71f61fdf248f2f9c28c53967b5
HoanDo <sieunhannhenkutesn@gmail.com>
2020-08-22 15:35:27 +0700

    update home screen

--- src/constant/image.js
+++ src/constant/image.js
@@ -1,11 +1,21 @@
-import Logo from '../../assets/image/Logo.svg'
-
+import Logo from '../../assets/image/Logo.svg';
+import Camera from '../../assets/image/camera.svg';
+import Box from '../../assets/image/box.svg';
+import Feedback from '../../assets/image/feedback.svg';
+import Speaker from '../../assets/image/speaker.svg';
+import Chopsticks from '../../assets/image/chopsticks.svg';
 const ImageStore = {
     png:{
         background : require('../../assets/image/background.png'),
     },
     svg:{
-        logo : Logo
+        logo : Logo,
+        camera : Camera,
+        box : Box,
+        feedback : Feedback,
+        speaker : Speaker,
+        chopsticks : Chopsticks,
+        
     }
     
 }
--- src/screen/Home.js
+++ src/screen/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {StyleSheet,ImageBackground,View,ScrollView} from 'react-native';
+import {StyleSheet,ImageBackground,View,ScrollView,TouchableOpacity,
+    Animated
+} from 'react-native';
 import Styles from '../style/home'
 import {Block,Text,Card} from '../component/index'
 import Image from '../constant/image'
 import { Dimensions } from "react-native";
-
-
+import CameraButton from '../component/CameraButton'
+import  StoreButton from '../component/StoreButton'
+import ReportButton from '../component/ReportButton'
+import Sound from '../component/Sound';
 export default function HomeScreen(){
     return(
         <Block flex>
@@ -14,18 +18,27 @@ export default function HomeScreen(){
             style={styles.image}
             >
                 <View style={styles.wrapperContentContainer}>
-                    <View style={styles.wrapperContent}>                       
+                    <View style={styles.wrapperContent}>    
+                        <TouchableOpacity>
+                            <Image.svg.chopsticks width={120} height={120}/>
+                        </TouchableOpacity>
+                        <Text style={styles.font}>Chopsticks</Text>
+                        <Sound/>                   
                     </View>
                 </View>
                 <View style={styles.wrapperListContainer}>
                     <ScrollView
                         horizontal={true}
                         showsHorizontalScrollIndicator={false}
-                        bouncesZoom={true}
+                        showsVerticalScrollIndicator={false}
+                        directionalLockEnabled
                     >
-                        <View style={styles.wrapperList}>
-
-                        </View>
+                        <TouchableOpacity>         
+                            <View style={styles.wrapperList}>
+                                <Image.svg.chopsticks width={80} height={80}/>
+                                <Text style={styles.fontList}>Chopsticks</Text>
+                            </View>
+                        </TouchableOpacity>
                         <View style={styles.wrapperList}>
 
                         </View>
@@ -34,6 +47,12 @@ export default function HomeScreen(){
                         </View>
                     </ScrollView>
                    
+                </View>
+                <View style={styles.wrapperFeature}>
+   
+                    <StoreButton/>
+                    <CameraButton/>
+                    <ReportButton/>
                 </View>
                 
 
@@ -47,12 +66,28 @@ export default function HomeScreen(){
 }
 const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
+    font : {
+        fontFamily : "NunitoBold",
+        fontSize : 42,
+        color : "#fff"
+    },
+    fontList : {
+        fontFamily : "NunitoBold",
+        fontSize : 18,
+        color : "#fff",
+        marginTop : 10,
+    },
+
     wrapperContentContainer: {
         flexDirection:'column',
         justifyContent: 'space-between',
         alignItems: 'center'
     },
     wrapperContent:{
+        flexDirection:'column',
+        padding : 20,
+        alignItems: 'center',
+        justifyContent : "center",
         width: width * 14 / 15,
         height: height / 2,
         backgroundColor:"#163172",
@@ -71,19 +106,30 @@ const styles = StyleSheet.create({
     wrapperListContainer:{
         flexDirection: "row",
         width: "100%",
-        height: "30%",
+        height: "auto",
         alignItems : "center",
+        marginTop : 10
         
     },
     wrapperList:{
+        flexDirection:'column',
+        padding : 20,
+        alignItems: 'center',
+        justifyContent : "center",
         width: 150,
         height : 183,
         backgroundColor:"#163172",
         borderRadius: 12,
         marginRight : 10
-
-
     },
+    wrapperFeature : {
+        flexDirection:'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width : "100%",
+        height :"auto",
+        marginTop : 15
+    },  
     image: {
       flex: 1,
       resizeMode: "cover",
